bfc1dbff7f14f13dfcbca2dc81477da518deff5d
muthurajendiran10 <muthurajendiran10@gmail.com>
2026-07-08 11:26:37 +0530

    Add files via upload

---
+++ javaScript.js
@@ -0,0 +1,17 @@
+console.log("JavaScript Basics");
+
+let userName = "Muthu";
+console.log(userName);
+console.log(typeof userName);
+let organization = "Deloitte";
+console.log(organization);
+console.log(typeof organization);
+let contactNumber = 1234567890;
+console.log(contactNumber);
+console.log(typeof contactNumber);
+let knowsAutomation = true;
+console.log(knowsAutomation);
+console.log(typeof knowsAutomation);
+let usesPlaywright;
+console.log(usesPlaywright);
+console.log(typeof usesPlaywright);
